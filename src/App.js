7e0432daa4e1559613ae0e32a016f568f94c2d89
Varun Sharma <61429136+varunsharma21@users.noreply.github.com>
2023-06-14 22:28:27 +0530

    Refactored App.js

--- src/App.js
+++ src/App.js
@@ -3,32 +3,8 @@ import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses.js";
 import NewExpense from "./components/NewExpense/NewExpense.js";
 
-const DUMMY_EXPENSES = [
-  // {
-  //   id: "e1",
-  //   title: "Toilet Paper",
-  //   amount: 94.12,
-  //   date: new Date(2020, 7, 14),
-  // },
-  // {
-  //   id: "e2",
-  //   title: "New TV",
-  //   amount: 799.49,
-  //   date: new Date(2021, 2, 12),
-  // },
-  // {
-  //   id: "e3",
-  //   title: "Car Insurance",
-  //   amount: 294.67,
-  //   date: new Date(2021, 2, 28),
-  // },
-  // {
-  //   id: "e4",
-  //   title: "New Desk (Wooden)",
-  //   amount: 450,
-  //   date: new Date(2021, 5, 12),
-  // },
-];
+// Empty(initially) array in which we will store the expenses added.
+const DUMMY_EXPENSES = [];
 
 function App() {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
